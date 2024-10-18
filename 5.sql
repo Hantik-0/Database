e7b35a7a03febae8de2b73666c9321b82bb269af
Hantik-0 <hantimirovratmir@gmail.com>
2024-10-18 23:46:35 +0300

    Update 5.sql

--- 5.sql
+++ 5.sql
@@ -27,26 +27,25 @@ order by
 	"EventTypeId" asc;
 
 select 
-	count(*) from (
-		select distinct 
-			u."FirstName", 
-			u."LastName", 
-			rnr."Email", 
-			rgs."RegistrationStatus"
-		from 
-			"User" u
-		JOIN 
-			"Role" r on u."RoleId" = r."RoleId" 
-		join 
-			"Runner" rnr on u."Email" = rnr."Email" 
-		join 
-			"Registration" reg on rnr."RunnerId" = reg."RunnerId"
-		join 
-			"RegistrationEvent" rei on reg."RegistrationId" = rei."RegistrationId"
-		join 
-			"Event" ev on rei."EventId" = ev."EventId" 
-		join 
-			"RegistrationStatus" rgs on reg."RegistrationStatusId" = rgs."RegistrationStatusId"  
-		where 
-			ev."EventTypeId" = 'FM' and rgs."RegistrationStatusId" = 1
-			) as "a"
+count(*) from (
+select distinct 
+	u."FirstName", 
+	u."LastName", 
+	rnr."Email", 
+	rgs."RegistrationStatus"
+from  "User" u
+JOIN 
+	"Role" r on u."RoleId" = r."RoleId" 
+join 
+	"Runner" rnr on u."Email" = rnr."Email" 
+join 
+	"Registration" reg on rnr."RunnerId" = reg."RunnerId"
+join 
+	"RegistrationEvent" rei on reg."RegistrationId" = rei."RegistrationId"
+join 
+	"Event" ev on rei."EventId" = ev."EventId" 
+join 
+	"RegistrationStatus" rgs on reg."RegistrationStatusId" = rgs."RegistrationStatusId"  
+where 
+	ev."EventTypeId" = 'FM' and rgs."RegistrationStatusId" = 1
+) as "a"
