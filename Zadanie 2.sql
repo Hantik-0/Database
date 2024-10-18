7eaf7b32c49f08423784162fbca717b682fe35a4
Hantik-0 <hantimirovratmir@gmail.com>
2024-10-18 23:19:53 +0300

    Add files via upload

---
+++ Zadanie 2.sql
@@ -0,0 +1,22 @@
+SELECT 
+    run."RunnerId",
+    gen."Gender",
+    extract(year from age(e."StartDateTime", run."DateOfBirth")) AS "AgeDuringEvent",
+    e."EventId",
+    e."StartDateTime",
+    rer."RaceTime"
+FROM 
+    "Runner" run
+join 
+	"Gender" gen ON run."GenderId" = gen."GenderId"
+join
+	"Registration" reg on run."RunnerId" = reg."RunnerId"
+JOIN 
+    "RegistrationEvent" rer ON reg."RegistrationId" = rer."RegistrationId"
+JOIN 
+    "Event" e ON rer."EventId" = e."EventId"
+WHERE 
+    e."StartDateTime" < NOW()
+ORDER BY 
+    run."RunnerId", e."StartDateTime";
+
